3fd0bb8327a35bc972fb3da8a080fe231a7ff312
appuser <appuser@journey3-79656846f7-k2dx9>
2026-01-25 14:43:02 +0100

    Initial commit

---
+++ boring-busses.js
@@ -0,0 +1,11 @@
+const busRoutes = [
+  ['B36', ['Hero Square', 'Forest Street', 'Castle Road', 'Bridge Bridge', 'Castle Road', 'Forest Street', 'Hero Square'], 3],
+  ['A12', ['Station A', 'Station B', 'Station C', 'Station B', 'Station A'], 2],
+  ['B24', ['Park Square', 'Lake Avenue', 'Park Square'], 1],
+  ['C08', ['Main Street', 'Oak Boulevard', 'Pine Avenue', 'Oak Boulevard', 'Main Street'], 2],
+  ['D45', ['Sunset Boulevard', 'Ocean Drive', 'Beach Avenue', 'Ocean Drive', 'Sunset Boulevard'], 2],
+  ['E56', ['Central Park', 'City Hall', 'Market Street', 'Liberty Avenue', 'Market Street', 'City Hall', 'Central Park',], 3],
+  ['F78', [ 'Museum Square', 'Art Street', 'Museum Square'], 1]
+];
+
+export { busRoutes };
---
+++ buses.js
@@ -0,0 +1,3 @@
+import * as fs from 'fs';
+import { busRoutes } from './boring-busses.js';
+import { departureTimes } from './timetables.js';
